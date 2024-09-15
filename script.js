d10eda1bb3a560a1607e8846b75d23d58dcb90f4
ThaboChauke <chaukethabo16@gmail.com>
2024-09-15 18:33:02 +0200

    displaying messages cards and sum

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
-
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("cards-el")
 
 function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardEl.textContent = "Cards: " + firstCard + " " + secondCard
+
     if (sum <= 20) {    
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "YAY! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackjack = true
     } else {
-        message = "You are out of the game 😭"
+        message = "You are out of the game"
         isAlive = false
     }
-    console.log(message)
+    messageEl.textContent = message
 }

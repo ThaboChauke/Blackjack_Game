92aeb796a41042387dacf7daaf646b85fcabc761
ThaboChauke <chaukethabo16@gmail.com>
2024-09-15 20:31:41 +0200

    use arrays for cards

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 1
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
@@ -10,8 +11,19 @@ let cardEl = document.getElementById("cards-el")
 
 
 function startGame() {
+    renderGame()
+}
+ 
+
+function renderGame() {
+    cardEl.textContent = "Cards: "
+
+    for (let card = 0; card < cards.length; card++) {
+        cardEl.textContent += cards[card] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
-    cardEl.textContent = "Cards: " + firstCard + " " + secondCard
+
 
     if (sum <= 20) {    
         message = "Do you want to draw a new card?"
@@ -25,12 +37,16 @@ function startGame() {
     messageEl.textContent = message
 }
 
+
 function newCard() {
     console.log("Drawing a new card from the deck!")
     let thirdCard = 5
-
     sum += thirdCard
+    cards.push(thirdCard)
 
-    startGame()
+    renderGame()
+}
 
+function getRandomCard() {
+    
 }

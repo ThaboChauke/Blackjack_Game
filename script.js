ba0b075a3781c8ac3f1d83951d720317622ce26f
ThaboChauke <chaukethabo16@gmail.com>
2024-09-15 17:34:56 +0200

    basic html structure and styling with background image

---
+++ script.js
@@ -0,0 +1,20 @@
+let firstCard = 10
+let secondCard = 1
+let sum = firstCard + secondCard
+let hasBlackjack = false
+let isAlive = true
+let message = ""
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+    message = "YAY! You've got Blackjack! 🥳"
+    hasBlackjack = true
+} else {
+    message = "You are out of the game 😭"
+    isAlive = false
+}
+
+console.log(hasBlackjack)
+console.log(isAlive)
+console.log(message)

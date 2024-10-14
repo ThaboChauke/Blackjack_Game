6b4e7eb5312b21c31dff7b0a06eedd334c497fad
ThaboChauke <chaukethabo16@gmail.com>
2024-10-14 20:55:02 +0200

    display cards

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let player = {
-    name : "Thabo",
-    chips : "11000"
+    name: "Thabo",
+    chips: "11000"
 }
 let cards = []
 let sum = 0
@@ -16,15 +16,29 @@ let playerEl = document.getElementById("player-el")
 playerEl.textContent = player.name + ": R" + player.chips
 
 function getRandomCard() {
+    // Generate a random card number between 1 and 13 (Ace to King)
     let randomNumber = Math.floor(Math.random() * 13) + 1
+    let cardValue = randomNumber > 10 ? 10 : randomNumber
+    let cardImage = ""
 
+    // Assign card image paths based on card number and suit
+    let suits = ["hearts", "diamonds", "spades", "clubs"]
+    let randomSuit = suits[Math.floor(Math.random() * 4)]
+    
     if (randomNumber === 1) {
-        return 11
-    } else if (randomNumber > 10) {
-        return 10
+        cardImage = "images/ace_of_" + randomSuit + ".png"
+        cardValue = 11
+    } else if (randomNumber === 11) {
+        cardImage = "images/jack_of_" + randomSuit + ".png"
+    } else if (randomNumber === 12) {
+        cardImage = "images/queen_of_" + randomSuit + ".png"
+    } else if (randomNumber === 13) {
+        cardImage = "images/king_of_" + randomSuit + ".png"
     } else {
-        return randomNumber
+        cardImage = "images/" + randomNumber + "_of_" + randomSuit + ".png"
     }
+
+    return { value: cardValue, image: cardImage }
 }
 
 function startGame() {
@@ -32,23 +46,26 @@ function startGame() {
 
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
-    sum = firstCard + secondCard
-    cards.push(firstCard,secondCard)
+    sum = firstCard.value + secondCard.value
+    cards = [firstCard, secondCard]
+    
     renderGame()
 }
- 
 
 function renderGame() {
-    cardEl.textContent = "Cards: "
+    cardEl.innerHTML = "Cards: "
 
-    for (let card = 0; card < cards.length; card++) {
-        cardEl.textContent += cards[card] + " "
+    // Render each card image
+    for (let i = 0; i < cards.length; i++) {
+        let cardImage = document.createElement("img")
+        cardImage.src = cards[i].image
+        cardImage.style.width = "100px"  // Set card width
+        cardEl.appendChild(cardImage)
     }
 
     sumEl.textContent = "Sum: " + sum
 
-
-    if (sum <= 20) {    
+    if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
@@ -60,13 +77,11 @@ function renderGame() {
     messageEl.textContent = message
 }
 
-
 function newCard() {
-    if (hasBlackjack === false && isAlive === true) {
-        let thirdCard = getRandomCard()
-        sum += thirdCard
-        cards.push(thirdCard)
+    if (!hasBlackjack && isAlive) {
+        let newCard = getRandomCard()
+        sum += newCard.value
+        cards.push(newCard)
         renderGame()
     }
 }
-
